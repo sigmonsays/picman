76f7c84c00af33f57b10b65b567f731ee004d1eb
Sig Lange <sig.lange@gmail.com>
2023-06-29 15:00:26 -0700

    just a minor tweak

--- autosort/processfile.go
+++ autosort/processfile.go
@@ -48,25 +48,3 @@ func (me *Autosort) ProcessFile(root, fullpath string, info fs.FileInfo, dstdir 
 
 	return nil
 }
-
-// year := date.Format("2006")
-// month := date.Format("01")
-// basename := filepath.Base(fullpath)
-// destpath := filepath.Join(dstdir, year, month, basename)
-
-// dstExists := false
-// dstSize := int64(0)
-// dst, err := os.Stat(destpath)
-// if err == nil {
-// 	dstExists = true
-// 	dstSize = dst.Size()
-// } else {
-// 	dstExists = false
-// }
-// sizeMismatch := srcSize != dstSize
-
-// if !dstExists {
-// 	log.Tracef(" destination exists: %v", dstExists)
-// }
-
-// log.Tracef("src:%s size:%d mismatch:%v destpath:%s", relpath, srcSize, sizeMismatch, destpath)
