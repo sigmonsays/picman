25b41f01975910a2cb7c601776ef7d9aaf7b4818
Sig Lange <sig.lange@gmail.com>
2023-06-28 22:02:30 -0700

    just a minor tweak

--- autosort.go
+++ autosort.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
 	"io"
 	"io/fs"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/urfave/cli/v2"
 )
@@ -18,7 +22,7 @@ type Autosort struct {
 
 func (me *Autosort) Flags() []cli.Flag {
 	incomingDir := "/data/Pictures-Android/AndroidDCIM/Camera"
-	destDir := "/data/Pictures-incoming"
+	destDir := "/data/Pictures"
 	ret := []cli.Flag{
 		&cli.StringFlag{
 			Name:    "src-dir",
@@ -93,20 +97,44 @@ func (me *Autosort) ProcessFile(root, fullpath string, info fs.FileInfo, dstdir 
 		return nil
 	}
 
-	// generate sha256
-	sha256 := ""
-	sha, err := Sha256File(fullpath)
+	relpath, err := filepath.Rel(root, fullpath)
 	if err != nil {
 		return err
 	}
-	sha256 = sha
-	log.Tracef("Sha256 %s: %s", fullpath, sha256)
+
+	srcSize := info.Size()
+
+	// determine destination path name
+	date, err := GetDate(fullpath)
+	if err != nil {
+		return err
+	}
+
+	year := date.Format("2006")
+	month := date.Format("01")
+	basename := filepath.Base(fullpath)
+	destpath := filepath.Join(dstdir, year, month, basename)
+
+	log.Tracef("src:%s size:%d destpath:%s", relpath, srcSize, destpath)
+
+	// do we need to convert it?
+	// heic ->  jpg
+
+	// generate sha256
+
+	// sha256 := ""
+	// sha, err := Sha256File(fullpath)
+	// if err != nil {
+	// 	return err
+	// }
+	// sha256 = sha
+	// log.Tracef("Sha256 %s: %s", fullpath, sha256)
 
 	return nil
 }
 
 var SupportedImageExt = map[string]bool{
-	".hcif": true,
+	".heic": true,
 	".jpeg": true,
 	".jpg":  true,
 	".png":  true,
@@ -152,4 +180,35 @@ func Sha256File(path string) (string, error) {
 	log.Tracef("checksum %s: %s %d bytes", path, shaStr, written)
 
 	return shaStr, nil
+}
+
+func GetDate(path string) (time.Time, error) {
+	args := []string{
+		"exiftool",
+		"-S",
+		"-DateTimeOriginal",
+		path,
+	}
+	none := time.Time{}
+	buf := bytes.NewBuffer(nil)
+	c := exec.Command(args[0], args[1:]...)
+	c.Stdout = buf
+	err := c.Run()
+	if err != nil {
+		return none, err
+	}
+	tmp := strings.Trim(buf.String(), "\n")
+	vals := strings.SplitN(tmp, ":", 2)
+	if len(vals) < 2 {
+		return none, fmt.Errorf("short length")
+	}
+	t := strings.Trim(vals[1], "\n\t ")
+
+	// 2023:03:02 18:13:31
+	tm, err := time.Parse("2006:01:02 15:04:05", t)
+	if len(vals) < 2 {
+		return none, fmt.Errorf("parse time %s: %s", t, err)
+	}
+
+	return tm, nil
 }
