7740bb3471f1dbe8de0e498056f18530e2bffa5e
Sig Lange <sig.lange@gmail.com>
2023-07-02 22:39:14 -0700

    output json

--- cleanup/processfile.go
+++ cleanup/processfile.go
@@ -1,20 +1,16 @@
 package cleanup
 
 import (
+	"encoding/json"
 	"fmt"
-	"strings"
 )
 
 func (me *Cleanup) ProcessFile(srcdir string, statefile string, opts *Options, stats *Stats) error {
 
 	result := RunCleanup(srcdir, statefile, opts, stats)
-	result.Finish()
 
-	if result.Row[0] == "OK" {
-		return nil
-	}
-
-	fmt.Printf("%s\n", strings.Join(result.Row, "\t"))
+	buf, _ := json.MarshalIndent(result, "", "  ")
+	fmt.Printf("%s\n", buf)
 
 	return nil
 }
--- cleanup/runcleanup.go
+++ cleanup/runcleanup.go
@@ -2,7 +2,6 @@ package cleanup
 
 import (
 	"os"
-	"path/filepath"
 
 	"github.com/sigmonsays/picman/core"
 )
@@ -29,60 +28,19 @@ var (
 	FileTypeNotSupported = NewReason("FileTypeNotSupported", "file type not supported")
 	MissingDate          = NewReason("MissingDate", "no date found")
 	SourceMissing        = NewReason("SourceMissing", "source missing")
-	//
-	// LE DE ND DNF DNP FTNS MD SM
-	// L D N F P NS D S
-	//
-	ReasonShortCodes = map[*Reason]string{
-		LoadError:            "L",
-		DestinationEmpty:     "E",
-		NoDestination:        "N",
-		DestinationIsNotFile: "F",
-		DoNotProcess:         "P",
-		FileTypeNotSupported: "T",
-		MissingDate:          "D",
-		SourceMissing:        "S",
-	}
 )
 
 type Result struct {
-	Reasons []*Reason
-	Row     []string
-}
-
-func (me *Result) Finish() {
-	expected := 5
-	if len(me.Row) == expected {
-		return
-	}
-	for i := 0; i < expected-len(me.Row); i++ {
-		me.Row = append(me.Row, "\t")
-	}
-}
-func (me *Result) WithReason(r *Reason) *Result {
-	me.Reasons = append(me.Reasons, r)
-	return me
-}
-func (me *Result) GetRow() []string {
-	row := make([]string, 0)
-	var reason *Reason
-	if len(me.Reasons) > 0 {
-		reason = me.Reasons[0]
-	}
-	if reason == nil {
-		row = append(row, "OK")
-	} else {
-		codes := ""
-		for _, r := range me.Reasons {
-			shortcode, ok := ReasonShortCodes[r]
-			if !ok {
-				continue
-			}
-			codes += shortcode
-		}
-		row = append(row, codes)
-	}
-	return row
+	OriginalFile         string
+	DestinationFile      string
+	LoadError            bool
+	DestinationEmpty     bool
+	NoDestination        bool
+	DestinationIsNotFile bool
+	DoNotProcess         bool
+	FileTypeNotSupported bool
+	MissingDate          bool
+	SourceMissing        bool
 }
 
 func RunCleanup(srcdir string, statefile string, opts *Options, stats *Stats) *Result {
@@ -95,56 +53,40 @@ func RunCleanup(srcdir string, statefile string, opts *Options, stats *Stats) *R
 	state := core.NewState()
 	err := state.Load(statefile)
 	if err != nil {
-		return ret.WithReason(LoadError)
+		ret.LoadError = true
+	}
+	if state == nil {
+		return nil
 	}
 
+	ret.OriginalFile = state.OriginalFilename
+	ret.DestinationFile = state.DestinationFilename
+
 	if state.DestinationFilename == "" {
-		ret.WithReason(DestinationEmpty)
+		ret.DestinationEmpty = true
 	}
 
 	// make sure the source exists and is a regular file
 	sourceExists := PathExistsRegularFile(state.OriginalFilename)
 	if !sourceExists {
-		ret.WithReason(SourceMissing)
+		ret.SourceMissing = true
 	}
 
 	// make sure the destination exists and is a regular file
 	destExists := PathExistsRegularFile(state.OriginalFilename)
 	if !destExists {
-		ret.WithReason(NoDestination)
+		ret.NoDestination = true
 	}
 
 	// check if our type is supported
 	if core.IsFileExtSupported(state.Ext) != nil {
-		ret.WithReason(FileTypeNotSupported)
+		ret.FileTypeNotSupported = true
 	}
 
 	// make sure we have a date
 	if state.Date == nil || (state.Date.Year == 0 || state.Date.Month == 0) {
-		ret.WithReason(MissingDate)
+		ret.MissingDate = true
 	}
-
-	// output the row
-	// - reason
-	// - ext
-	// - srcpath (relative)
-	// - dstpath (absolute)
-	row := ret.GetRow()
-
-	if state.Ext == "" {
-		row = append(row, "-")
-	} else {
-		row = append(row, state.Ext)
-
-	}
-
-	// src path
-	srcrel, _ := filepath.Rel(srcdir, state.OriginalFilename)
-	row = append(row, srcrel)
-
-	// dest path
-	row = append(row, state.DestinationFilename)
-	ret.Row = row
 
 	// todo: if DoNotProcess is set we just delete the metadata file
 	// maybe we need a --delete flag
