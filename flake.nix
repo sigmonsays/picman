f2a0550ca6e2aeb4bd3df41090dd214cc6255ccf
Sig Lange <sig.lange@gmail.com>
2023-12-20 09:35:16 -0800

    add docker image

--- flake.nix
+++ flake.nix
@@ -5,19 +5,37 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.gomod2nix.url = "github:nix-community/gomod2nix";
 
-  outputs = { self, nixpkgs, flake-utils, gomod2nix }:
-    (flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ gomod2nix.overlays.default ];
-          };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    gomod2nix,
+  }: (
+    flake-utils.lib.eachDefaultSystem
+    (system: let
 
-        in
-        {
-          packages.default = pkgs.callPackage ./. { };
-          devShells.default = import ./shell.nix { inherit pkgs; };
-        })
-    );
+
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [gomod2nix.overlays.default];
+      };
+
+      bin = pkgs.callPackage ./. {};
+
+      dockerImage = pkgs.dockerTools.buildImage {
+        name = "picman";
+        tag = "latest";
+        copyToRoot = [bin];
+        config = {
+          Cmd = ["${bin}/bin/picman"];
+        };
+      };
+    in with pkgs; {
+      packages = {
+        inherit bin dockerImage;
+        default = bin;
+      };
+      devShells.default = import ./shell.nix {inherit pkgs;};
+    })
+  );
 }
